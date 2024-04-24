e5b0c94da7de88d8ecb27a67754a07ba74ce57c5
PavithraKandavel <pkandavel@my.okcu.edu>
2024-04-24 11:13:01 -0500

    third

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/hw4/', 
-  server: {
-    open: true 
-  }
-})
+  base: '/hw4/',
+  plugins: [react()]
+});
